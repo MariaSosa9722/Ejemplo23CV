7b6029b45aa1ff783c7d69c32ece7e5ab8d00965
Majo Sosa <maria.sosa@anconaautopartes.com>
2022-07-08 19:25:59 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFinal-23cv/Context/AplicationdbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_23cv.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_23cv.Context
+{
+    public  class AplicationdbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySQL("server = localhost; database= proyectofin; user = root; password=");
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
